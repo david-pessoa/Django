d6171a0cb9499e2866a9e663a969ca8afacc2693
david-pessoa <pessoa.dvlbp@gmail.com>
2024-11-22 18:45:24 -0300

    add mudanças da aula 30

--- env/core/views.py
+++ env/core/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    produtos = Produto.objects.all()
+
+    context = {
+        'produtos': produtos
+    }
+    return render(request, 'teste.html', context) #index.html
 
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request, pk):
+    produto = Produto.objects.filter(pk=pk)
+    context = {'produto': produto}
+
+    return render(request, 'produto.html', context)
